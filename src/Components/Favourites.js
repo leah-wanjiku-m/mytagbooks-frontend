cad14a70e744b72864d32d511cd696990d073cc3
leah wanjiku <leagm22@gmail.com>
2023-03-03 20:28:14 +0300

    git add details

--- src/Components/Favourites.js
+++ src/Components/Favourites.js
@@ -1,24 +1,36 @@
 import React, { useState, useEffect } from 'react';
 
 function Favourites() {
-  const [favourites, setFavourites] = useState([]);
 
+
+  const [favourites, setFavourites] = useState([]);
   useEffect(() => {
     fetch("http://localhost:9292/favourites")
       .then(response => response.json())
       .then(data => setFavourites(data))
       .catch(error => console.log(error));
   }, []);
+  const handleDeleteClick = (id) => {
+    fetch(`http://localhost:9292/favourites/${id}`, {
+      method: "DELETE",
+    })
+      .then(response => response.json())
+      .then(data => {
+        const filteredFavourites = favourites.filter(favourite => favourite.id !== id);
+        setFavourites(filteredFavourites);
+      })
+      .catch(error => console.log(error));
+  };
+
 
   return (
     <div className="row">
       {favourites.map(favourite => (
-        <div key={favourite.book.id} className="card" style={{ width: "18rem" }}>
-          <img src={favourite.book.image_url} className="card-img-top" alt={favourite.book.title} />
+        <div key={favourite.id} className="card" style={{ width: "18rem" }}>
           <div className="card-body">
             <h5 className="card-title">{favourite.book.title}</h5>
             <p className="card-text">{favourite.book.description}</p>
-            <p className="card-text">Added by: {favourite.user.email}</p>
+            <button className="btn btn-danger" onClick={() => handleDeleteClick(favourite.id)}>Delete</button>
           </div>
         </div>
       ))}
