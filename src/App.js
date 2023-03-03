5e72243f86032e21d24c20610b56ea41a25e32ab
leah wanjiku <leagm22@gmail.com>
2023-03-03 20:27:54 +0300

    git add

--- src/App.js
+++ src/App.js
@@ -8,25 +8,20 @@ import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [books, setBooks] = useState([]);
-  const [favorites, setFavorites] = useState([]);
+  // const [favorites, setFavorites] = useState([]);
   useEffect(()=>{
     fetch("/books")
    .then((response)=>response.json())
    .then((data)=>setBooks(data))
-// },[]);
-fetch('/favourites')
-.then(response => response.json())
-.then(data => setFavorites(data))
-.catch(error => console.log(error));
-}, []);
+ },[]);
 
   return (
     <div >
       <Navbar/>
       <Routes>
-    <Route path='/books' element={<Books books={books} favorites={favorites}/>}/>
+    <Route path='/books' element={<Books books={books} />}/>
     <Route path='/search' element={<Search />}/>
-     <Route path='/favourites' element={<Favourites  books={books} favorites={favorites} />}/>
+     <Route path='/favourites' element={<Favourites  books={books}  />}/>
       </Routes>
     </div>
      );
